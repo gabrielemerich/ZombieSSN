80ee6a7d93b92f499009d8d2f266a29a20c5e3b2
Gabriel Emerich <glemerich@gmail.com>
2020-07-03 23:18:32 -0300

    Change url api to https

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://zssn-backend-example.herokuapp.com/api/'
+    baseURL: 'https://zssn-backend-example.herokuapp.com/api/'
 });
 
